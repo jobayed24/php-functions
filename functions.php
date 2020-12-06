30954f66af24ac7ef4be9bd0624bc48e0d5c3264
jobayed24 <jkhouse117@gmail.com>
2020-12-06 20:18:03 +0600

    find out factorials and practicing Encapsulation functions

--- functions.php
+++ functions.php
@@ -1,11 +1,6 @@
 <?php
 
-function isEven($n){
-    if($n%2==0){
-        return true;   
-    }   
-    return false;
-}
+
 
 function factorials($n){
     $result=1;
--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 require_once "functions.php";
 
-$x=7;   
-if(isEven($x)){
-    echo "{$x} this is Even Number";
-    
-}else{
-    echo "{$x} this number is odd";
-}
+$x=5;   
+echo factorials($x);
