8390b217a9877d59c7be70b05e1761c8bb09e0dc
jobayed24 <jkhouse117@gmail.com>
2020-12-06 23:08:07 +0600

    variable scop global,local,and static scop

--- functions.php
+++ functions.php
@@ -1,25 +1,31 @@
 <?php
 
-///recursive functions full proccess called recusion ....
+////variable global scop ,local scop and static scop
 
-function printN($i){
-    if($i>10){
-        return;
-    }
+$name=3;
+function doSomething(){
+    echo $GLOBALS['name'];      //fucntions outside varaible when use in functions it's called global variable when you use  
+    //but when you can't use $GLOBALS scop then you use global scop
     
-    
+}
+
+//doSomething();
+
+function localScop(){
+    global $jk;
+    $jk="jobayed"; //local Scop when i want to use variable value from a functions it's need to make use Scop ....
+
+}
+//localScop();
+//echo $jk;
+
+function sta(){      ///static scop is very important for store value in every time functions call
+    static $i;
+    $i=$i ?? 0;
     echo "$i \n";
     $i++;
-    printN($i);
 }
-
-function startToEnd($sart,$end,$steping=2){
-    if($sart>$end){
-        return;
-    }
-    echo "$sart \n";
-    $sart+=$steping;
-    startToEnd($sart,$end,$steping);
-}
-
-startToEnd(3,55,6);
+sta();
+sta();
+sta();
+sta();
