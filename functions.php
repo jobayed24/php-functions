aebf9f9939b92001a3ad14c20e5259b7adf6275a
jobayed24 <jkhouse117@gmail.com>
2020-12-06 23:31:27 +0600

    find out factorials with recursive functions

--- functions.php
+++ functions.php
@@ -1,31 +1,12 @@
 <?php
 
-////variable global scop ,local scop and static scop
+///factorails find out by recursive functions remember that recursive fucntions mean it's called by himself
 
-$name=3;
-function doSomething(){
-    echo $GLOBALS['name'];      //fucntions outside varaible when use in functions it's called global variable when you use  
-    //but when you can't use $GLOBALS scop then you use global scop
+function fc($i){
+    if($i<1){
+        return 1;
+    }
+  return $i * fc($i-1);
     
 }
-
-//doSomething();
-
-function localScop(){
-    global $jk;
-    $jk="jobayed"; //local Scop when i want to use variable value from a functions it's need to make use Scop ....
-
-}
-//localScop();
-//echo $jk;
-
-function sta(){      ///static scop is very important for store value in every time functions call
-    static $i;
-    $i=$i ?? 0;
-    echo "$i \n";
-    $i++;
-}
-sta();
-sta();
-sta();
-sta();
+echo fc(5);
